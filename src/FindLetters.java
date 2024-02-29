c239aee9c03442daa74172bf1c25c670ebec507b
leejonghyun93 <fhohffodf93@gmail.com>
2024-02-29 15:36:29 +0900

    1-1 문자 찾기

--- src/FindLetters.java
+++ src/FindLetters.java
@@ -1,40 +1,33 @@
 import java.util.*;
-
+//1.문자 찾기
+//        설명
+//        한 개의 문자열을 입력받고,특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+//        대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
+//        입력
+//        첫 줄에 문자열이 주어지고,두 번째 줄에 문자가 주어진다.
+//        문자열은 영어 알파벳으로만 구성되어 있습니다.
+//        출력
+//        첫 줄에 해당 문자의 개수를 출력한다.
 
 class FindLetters {
-    public boolean isPrime(int num){
-        if(num == 1) return false;
-        for(int i=2; i< num; i++){
-            if(num%i == 0) return false;
-        }
-        return true;
-    }
-
-    public ArrayList<Integer> solution(int n,int[] arr){
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            int tmp = arr[i];
-            int res = 0;
-            while(tmp>0){
-                int t = tmp % 10;
-                res = res * 10 +t;
-                tmp = tmp / 10;
-            }
-            if(isPrime(res)) answer.add(res);
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        //  System.out.println(str+ " "+t);
+        for(int i =0; i < str.length(); i++){
+            if(str.charAt(i)==t) answer++;
         }
         return answer;
     }
 
+
     public static void main(String[] args) {
         FindLetters T = new FindLetters();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = kb.nextInt();
-        }
-        for(int x : T.solution(n,arr)){
-            System.out.print(x+" ");
-        }
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.print(T.solution(str, c));
     }
 }
